91fc4a2a150dfe83054804b140f5e917cd5fbf55
Becka <rebecca.lelew@gmail.com>
2022-09-13 20:41:39 +0100

    Implement other translated commands - buggy atm

--- project-7/vm-translator/src/main/scala/parser/Commands.scala
+++ project-7/vm-translator/src/main/scala/parser/Commands.scala
@@ -5,13 +5,13 @@ sealed trait Command
 sealed trait ArithmeticAndLogicalCommand extends Command
 case object Add extends ArithmeticAndLogicalCommand
 case object Sub extends ArithmeticAndLogicalCommand
-case object Neg extends ArithmeticAndLogicalCommand
 case object Eq extends ArithmeticAndLogicalCommand
 case object Gt extends ArithmeticAndLogicalCommand
 case object Lt extends ArithmeticAndLogicalCommand
 case object And extends ArithmeticAndLogicalCommand
 case object Or extends ArithmeticAndLogicalCommand
 case object Not extends ArithmeticAndLogicalCommand
+case object Neg extends ArithmeticAndLogicalCommand
 
 sealed trait MemoryCommand extends Command
 case class Pop(memorySegment: MemorySegment, i: Int) extends MemoryCommand
--- project-7/vm-translator/src/main/scala/parser/Translator.scala
+++ project-7/vm-translator/src/main/scala/parser/Translator.scala
@@ -25,21 +25,53 @@ object Translator {
 
   def translateArithmeticAndLogicalCommand(command: ArithmeticAndLogicalCommand): List[String] =
     command match {
-      case Add => ("//add" +: setDAndMRegistersToTopTwoStackValues) ++ ("M=D+M" +: setSPToCurrentAddressPlusOne)
-      case Sub => ("//sub" +: setDAndMRegistersToTopTwoStackValues) ++ ("M=D-M" +: setSPToCurrentAddressPlusOne)
-      case _ => ???
+      case Add => binaryOperation(List("M=D+M"), "//add")
+      case Sub => binaryOperation(List("M=D-M"), "//sub")
+      case Gt => binaryOperation(jumpLogicForComparators("JGT"), "//gt")
+      case Lt => binaryOperation(jumpLogicForComparators("JLT"), "//lt")
+      case Eq => binaryOperation(jumpLogicForComparators("JEQ"), "//eq")
+      case And => binaryOperation(List("M=M&D"), "//and")
+      case Or => binaryOperation(List("M=D|M"), "//or")
+      case Neg => unaryOperation(List("M=-M"), "//neg")
+      case Not => unaryOperation(List("M=!M"), "//neg")
     }
 
-  private val setSPToCurrentAddressPlusOne = List(
-    "D=A",
-    "@SP",
-    "M=D+1"
-  )
+  private def unaryOperation(setM: List[String], commentString: String): List[String] = {
+    val getMOfTopStackValue = List(
+      "@SP",
+      "A=M-1",
+    )
+    //is this right and the one below?
+    val setSPToCurrentAddressPlusOne = List(
+      "D=A",
+      "@SP",
+      "M=D+1"
+    )
+    (commentString +: getMOfTopStackValue) ++ setM ++ setSPToCurrentAddressPlusOne
+  }
 
-  private val setDAndMRegistersToTopTwoStackValues =  List(
-    "@SP",
-    "A=M-1",
-    "D=M",
-    "A=A-1",
+
+  private def binaryOperation(setMTo: List[String], commentString: String): List[String] = {
+    val setDAndMRegistersToTopTwoStackValues =  List(
+      "@SP",
+      "A=M-1",
+      "D=M",
+      "A=A-1",
+    )
+    val setSPToCurrentAddressPlusOne = List(
+      "D=A",
+      "@SP",
+      "M=D+1"
+    )
+    (commentString +: setDAndMRegistersToTopTwoStackValues) ++ setMTo ++ setSPToCurrentAddressPlusOne
+  }
+
+  private def jumpLogicForComparators(jumpString: String) = List(
+    "D=D-M",
+    "@SET_M_TO_TRUE",
+    s"D;$jumpString",
+    "M=0",
+    "(SET_M_TO_TRUE)",
+    "M=1",
   )
 }
