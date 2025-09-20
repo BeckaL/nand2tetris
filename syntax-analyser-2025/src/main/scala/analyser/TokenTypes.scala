a6abb4183dd7b1cc8f4820a357d43267b2a697f6
BeckaL <beckal@users.noreply.github.com>
2025-09-20 16:17:57 +0100

    copy across tokeniser from second attempt

---
+++ syntax-analyser-2025/src/main/scala/analyser/TokenTypes.scala
@@ -0,0 +1,40 @@
+package analyser
+
+object TokenTypes extends Enumeration {
+  type TokenType = Value
+
+  val Keyword, Symbol, Identifier, IntConst, StringConst = Value
+
+  val SUBROUTINE_RETURN_TYPES: Set[String] = Set("void", "int", "char", "boolean")
+  val SUBROUTINE_TYPES: Set[String] = Set("constructor", "function", "method")
+  val CLASS_VAR_TYPES: Set[String] = Set("static", "field")
+  val KEYWORD_CONSTANTS: Set[String] = Set("true", "false", "null", "this")
+  val UNARY_OPERATORS: Set[String] = Set("-", "~")
+  val OPERATORS: Set[String] = Set("+", "-", "*", "/", "&", "|", "<", ">", "=")
+
+  def tokenType(currentToken: String): TokenType =
+    if (keywordsRegex.matches(currentToken))
+      Keyword
+    else if (symbols.contains(currentToken))
+      Symbol
+    else if (integerRegex.matches(currentToken) && currentToken.toInt <= 32767)
+      IntConst
+    else if (stringConstRegex.matches(currentToken))
+      StringConst
+    else if (identifierRegex.matches(currentToken))
+      Identifier
+    else
+      throw new RuntimeException(s"Couldn't parse token: $currentToken is not a valid token")
+
+  val keywordsRegex =
+    ("class|constructor|function|method|field|static|var|int|char|" +
+      "boolean|void|true|false|null|this|let|do|if|else|while|return").r
+
+  val symbols = "{}()[].,;+-*/&|<>=~".split("").toList
+  
+  val integerRegex = "^[1-9]\\d{0,4}|0$".r
+
+  val stringConstRegex = "^\"[^\"\\n]*\"$".r
+
+  val identifierRegex = "^[a-zA-Z_][a-zA-Z1-9_]*$".r
+}
---
+++ syntax-analyser-2025/src/main/scala/analyser/Tokeniser.scala
@@ -0,0 +1,49 @@
+package analyser
+
+import scala.util.matching.Regex
+
+trait Tokeniser {
+  def advance(): Unit
+  def currentToken: String
+  def hasMoreTokens: Boolean
+}
+
+//At this point, input should be a string with only spaces as whitespace, newlines etc replaced by spaces
+class DefaultTokeniser(input: String, var position: Int = 0, var currentToken: String = "") extends Tokeniser {
+  def hasMoreTokens: Boolean =
+    position != -1 && position < input.size && input.slice(position, input.size).exists(!_.isSpaceChar)
+
+  def advance(): Unit = {
+    val nextPosition = input.indexWhere(_ != ' ', position)
+    if (input.indices.contains(nextPosition)) {
+      val firstChar = input(nextPosition)
+      val (regexToFind, offset) = terminatingRegexAndOffset(firstChar)
+      val searchStart = if (nextPosition == input.indices.max) nextPosition else nextPosition + 1
+      position = input.indexWhere(c => regexToFind.matches(c.toString), searchStart) + offset
+      if (position == nextPosition && position == input.indices.max)
+        position = position + 1
+      currentToken = input.slice(nextPosition, position)
+    } else {
+      currentToken = ""
+    }
+  }
+
+  def keyword: String = currentToken
+  def symbol: Char = currentToken.charAt(0)
+  def identifier: String = currentToken
+  def intVal: Int = currentToken.toInt
+  def stringVal: String = currentToken.tail.dropRight(1)
+
+  private def terminatingRegexAndOffset(firstChar: Char): (Regex, Int) =
+    if (TokenTypes.symbols.contains(firstChar.toString))
+      (".".r, 0)
+    else if (firstChar == '"')
+      ("\"".r, 1) //offset is bigger here because we want to include the closing double quote in the string
+    else if (firstChar.isDigit)
+      ("[^\\d]".r, 0)
+    else
+      ("[^a-zA-Z_]".r, 0)
+}
+
+
+
