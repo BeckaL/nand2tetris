33a5ea1e1b4bcadd51dcf4ddcb3362b1e361eb64
BeckaL <beckal@users.noreply.github.com>
2025-09-20 16:18:56 +0100

    remove unnecessary trait

--- syntax-analyser-2025/src/main/scala/analyser/CompilationEngine.scala
+++ syntax-analyser-2025/src/main/scala/analyser/CompilationEngine.scala
@@ -5,24 +5,24 @@ import scala.util.chaining.scalaUtilChainingOps
 
 object CompilationEngine {
   def compileLet(t: Tokeniser): MaybeLexicalElements =
-    val optionalExpressionRule = OptionalElemRule(_ == "[", ObjectRule(List(SymbolRule("["), CustomRule(compileExpression), SymbolRule("]"))).compile, false)
-    val rules = List(KeywordRule("let"), VarRule, optionalExpressionRule, SymbolRule("="), CustomRule(compileExpression), semicolon)
+    val optionalExpressionRule = optionalElemRule(_ == "[", objectRule(List(symbolRule("["), compileExpression, symbolRule("]"))), false)
+    val rules = List(keywordRule("let"), varRule, optionalExpressionRule, symbolRule("="), compileExpression, semicolon)
     compileWithRules(t, rules, Some("letStatement"))
 
   def compileClass(t: Tokeniser): MaybeLexicalElements = {
-    val terminatingClosingBracket = CustomRule(tk =>
+    val terminatingClosingBracket = (tk: Tokeniser) => {
       val moreTokensWarning = "uh oh tokens after class has closed"
       if (tk.currentToken == "}")
         if (tk.hasMoreTokens) Left(moreTokensWarning) else Right(List(Symbol('}')))
       else
         Left(s"expected closing char } for class")
-    )
+    }
     val rules = List(
-      KeywordRule("class"),
-      VarRule,
+      keywordRule("class"),
+      varRule,
       openCurlyBracket,
-      ZeroOrMoreRule(List("static", "field"), compileClassVarDec),
-      ZeroOrMoreRule(List("function", "method", "constructor"), compileSubroutine),
+      zeroOrMoreRule(List("static", "field"), compileClassVarDec),
+      zeroOrMoreRule(List("function", "method", "constructor"), compileSubroutine),
       terminatingClosingBracket
     )
     compileWithRules(t, rules, Some("class"))
@@ -30,18 +30,18 @@ object CompilationEngine {
 
   def compileClassVarDec(t: Tokeniser): MaybeLexicalElements =
     val rules =
-      List(KeywordMatchingOneOfRule(List("static", "field")), TypeRule(), CustomRule(parseNVars(_)), semicolon)
+      List(keywordMatchingOneOfRule(List("static", "field")), typeRule(), parseNVars(_), semicolon)
     compileWithRules(t, rules, Some("classVarDec"))
 
   def compileSubroutine(t: Tokeniser): MaybeLexicalElements = {
     val rules = List(
-      KeywordMatchingOneOfRule(List("function", "method", "constructor")),
-      TypeRule(includeVoid = true),
-      VarRule,
+      keywordMatchingOneOfRule(List("function", "method", "constructor")),
+      typeRule(includeVoid = true),
+      varRule,
       openBracket,
-      CustomRule(compileParameterList(_, ")")),
+      compileParameterList(_, ")"),
       closeBracket,
-      CustomRule(compileSubroutineBody)
+      compileSubroutineBody
     )
     compileWithRules(t, rules, Some("subroutineDec"))
   }
@@ -52,8 +52,8 @@ object CompilationEngine {
       Right(encloseWithTags("parameterList", elemsSoFar))
     } else {
       val result = for {
-        varType <- TypeRule(includeVoid = true).compile(t)
-        varNameLexElems <- VarRule.compile(t)
+        varType <- typeRule(includeVoid = true)(t)
+        varNameLexElems <- varRule(t)
       } yield varType ++ varNameLexElems
       result match {
         case Left(str) => Left(str)
@@ -72,47 +72,47 @@ object CompilationEngine {
     }
 
   def compileSubroutineBody(t: Tokeniser): MaybeLexicalElements =
-    val rules = List(openCurlyBracket, ZeroOrMoreRule(List("var"), compileVarDec), CustomRule(compileStatements(_, "}")), closeCurlyBracket)
+    val rules = List(openCurlyBracket, zeroOrMoreRule(List("var"), compileVarDec), compileStatements(_, "}"), closeCurlyBracket)
     compileWithRules(t, rules, Some("subroutineBody"))
 
   private def encloseWithTags(tagname: String, elems: List[LexicalElem]) =
     StartElem(tagname) +: elems :+ EndElem(tagname)
 
   def compileVarDec(t: Tokeniser): MaybeLexicalElements =
-    val rules = List(KeywordRule("var"), TypeRule(), CustomRule(parseNVars(_)), semicolon)
+    val rules = List(keywordRule("var"), typeRule(), parseNVars(_), semicolon)
     compileWithRules(t, rules, Some("varDec"))
 
   //TODO write tests
   def compileStatements(t: Tokeniser, terminatingString: String, soFar: List[LexicalElem] = List()): MaybeLexicalElements =
     val ruleMap =
       Map("let" -> compileLet, "do" -> compileDo, "while" -> compileWhile, "if" -> compileIf, "return" -> compileReturn)
-    compileWithRules(t, List(ZeroOrMoreRule(ruleMap.keys.toList, BranchingRule(ruleMap).compile)), Some("statements"))
+    compileWithRules(t, List(zeroOrMoreRule(ruleMap.keys.toList, branchingRule(ruleMap))), Some("statements"))
 
   def compileIf(t: Tokeniser): MaybeLexicalElements =
     val rules = List(
-      KeywordRule("if"),
+      keywordRule("if"),
       openBracket,
-      CustomRule(compileExpression),
+      compileExpression,
       closeBracket,
       statementsEnclosedByCurlies,
-      OptionalElemRule(_ == "else", (t: Tokeniser) => statementsEnclosedByCurlies.compile(t).map(Keyword("else") +: _))
+      optionalElemRule(_ == "else", (t: Tokeniser) => statementsEnclosedByCurlies(t).map(Keyword("else") +: _))
     )
     compileWithRules(t, rules, Some("ifStatement"))
 
-  val statementsEnclosedByCurlies = ObjectRule(List(openCurlyBracket, CustomRule(compileStatements(_, "}")), closeCurlyBracket))
+  val statementsEnclosedByCurlies = objectRule(List(openCurlyBracket, compileStatements(_, "}"), closeCurlyBracket))
 
   def compileWhile(t: Tokeniser): MaybeLexicalElements =
-    val rules = List(KeywordRule("while"), openBracket, CustomRule(compileExpression), closeBracket, statementsEnclosedByCurlies)
+    val rules = List(keywordRule("while"), openBracket, compileExpression, closeBracket, statementsEnclosedByCurlies)
     compileWithRules(t, rules, Some("whileStatement"))
 
   def compileDo(t: Tokeniser): MaybeLexicalElements = {
-    val doRules = List(KeywordRule("do"), ObjectRule(subroutineCallRules), semicolon)
+    val doRules = List(keywordRule("do"), objectRule(subroutineCallRules), semicolon)
     compileWithRules(t, doRules, Some("doStatement"))
   }
 
   val optionalDotCallRule = (t: Tokeniser) =>
     if (t.currentToken == ".") {
-      t.safeAdvance.flatMap(_ => VarRule.compile(t).map(v2 => Symbol('.') +: v2))
+      t.safeAdvance.flatMap(_ => varRule(t).map(v2 => Symbol('.') +: v2))
     } else if (t.currentToken == "(") {
       Right(List())
     } else {
@@ -120,7 +120,7 @@ object CompilationEngine {
     }
 
   val subroutineCallRules =
-    List(VarRule, CustomRule(optionalDotCallRule), openBracket, ExpressionListRule, closeBracket)
+    List(varRule, optionalDotCallRule, openBracket, expressionListRule, closeBracket)
 
   private def parseSubroutineCall(t: Tokeniser) =
     compileWithRules(t, subroutineCallRules, None)
@@ -129,15 +129,15 @@ object CompilationEngine {
     compileWithRules(t, subroutineCallRules.tail, None).map(elems => v +: elems)
 
   def compileReturn(t: Tokeniser): MaybeLexicalElements =
-    val optionalExpressionRule = OptionalElemRule(_ != ";", CustomRule(compileExpression).compile, advanceIfConditionMet = false)
+    val optionalExpressionRule = optionalElemRule(_ != ";", compileExpression, advanceIfConditionMet = false)
 
-    val rules = List(KeywordRule("return"), optionalExpressionRule, semicolon)
+    val rules = List(keywordRule("return"), optionalExpressionRule, semicolon)
     compileWithRules(t, rules, Some("returnStatement"))
 
   def compileExpression(t: Tokeniser): MaybeLexicalElements =
     val rules = List(
-      CustomRule(compileTerm),
-      OptionalElemRule(TokenTypes.OPERATORS.contains, ObjectRule(List(SymbolMatchingOneOfRule(TokenTypes.OPERATORS.toList), CustomRule(compileTerm))).compile, false))
+      compileTerm,
+      optionalElemRule(TokenTypes.OPERATORS.contains, objectRule(List(symbolMatchingOneOfRule(TokenTypes.OPERATORS.toList), compileTerm)), false))
     compileWithRules(t, rules, Some("expression"))
 
   def compileExpressionList(t: Tokeniser): Either[String, (List[LexicalElem], Int)] =
@@ -158,8 +158,8 @@ object CompilationEngine {
 
     go(List(), 0)
 
-  val compileTermRule: CompilationRule = CustomRule(
-    (t: Tokeniser) =>
+  val compileTermRule: RuleTransformer = 
+    (t: Tokeniser) => {
       val s = t.currentToken
       TokenTypes.tokenType(s) match
         case TokenTypes.Identifier =>
@@ -168,7 +168,7 @@ object CompilationEngine {
             t.currentToken match {
               case currentToken if List("(", ".").contains(currentToken) => parseSubroutineCallFromDot(t, currentElem)
               case "[" =>
-                val rules = List(SymbolRule("["), CustomRule(compileExpression), SymbolRule("]"))
+                val rules = List(symbolRule("["), compileExpression, symbolRule("]"))
                 compileWithRules(t, rules, None).map(elems => currentElem +: elems)
               case _ => Right(List(currentElem))
             }
@@ -177,12 +177,12 @@ object CompilationEngine {
         case TokenTypes.Keyword if TokenTypes.KEYWORD_CONSTANTS.contains(s) => Right(List(Keyword(s))).tap(_ => t.advance())
         case TokenTypes.StringConst => Right(List(stringConstFromQuotedString(s))).tap(_ => t.advance())
         case TokenTypes.Symbol if TokenTypes.UNARY_OPERATORS.contains(s) =>
-          compileWithRules(t, List(SymbolMatchingOneOfRule(TokenTypes.UNARY_OPERATORS.toList), CustomRule(compileTerm)), None)
+          compileWithRules(t, List(symbolMatchingOneOfRule(TokenTypes.UNARY_OPERATORS.toList), compileTerm), None)
         case TokenTypes.Symbol if s == "(" =>
-          val rules = List(SymbolRule("("), CustomRule(compileExpression), SymbolRule(")"))
+          val rules = List(symbolRule("("), compileExpression, symbolRule(")"))
           compileWithRules(t, rules, None)
         case otherTokenType => Left(s"expected valid term to be found in string $s but got $otherTokenType")
-  )
+    }
 
   def compileTerm(t: Tokeniser): MaybeLexicalElements =
     compileWithRules(t, List(compileTermRule), Some("term"))
@@ -191,7 +191,7 @@ object CompilationEngine {
     StringConst(s.tail.dropRight(1))
 
   private def parseNVars(t: Tokeniser, soFar: List[LexicalElem] = List()): MaybeLexicalElements =
-    VarRule.compile(t).flatMap(varNameLexElem =>
+    varRule(t).flatMap(varNameLexElem =>
       t.currentToken match {
         case ";" => Right(soFar ++ varNameLexElem)
         case "," => t.safeAdvance.flatMap(_ =>
--- syntax-analyser-2025/src/main/scala/analyser/CompilationRule.scala
+++ syntax-analyser-2025/src/main/scala/analyser/CompilationRule.scala
@@ -7,25 +7,17 @@ import scala.util.chaining.scalaUtilChainingOps
 
 type MaybeLexicalElements = Either[String, List[LexicalElem]]
 
-sealed trait CompilationRule {
-  def compile(t: Tokeniser): MaybeLexicalElements
-}
+def keywordRule(stringToMatch: String)(t: Tokeniser): MaybeLexicalElements =
+  stringMatchingRule(t, List(stringToMatch), Keyword.apply)
 
-case class KeywordRule(stringToMatch: String) extends CompilationRule {
-  override def compile(t: Tokeniser): MaybeLexicalElements =
-    stringMatchingRule(t, List(stringToMatch), Keyword.apply)
-}
+def symbolRule(stringToMatch: String)(t: Tokeniser) =
+  stringMatchingRule(t, List(stringToMatch), (s: String) => Symbol(s.head))
 
-case class SymbolRule(stringToMatch: String) extends CompilationRule {
-  override def compile(t: Tokeniser): MaybeLexicalElements =
-    stringMatchingRule(t, List(stringToMatch), (s: String) => Symbol(s.head))
-}
-
-val openBracket = SymbolRule("(")
-val openCurlyBracket = SymbolRule("{")
-val closeBracket = SymbolRule(")")
-val closeCurlyBracket = SymbolRule("}")
-val semicolon = SymbolRule(";")
+val openBracket = symbolRule("(")
+val openCurlyBracket = symbolRule("{")
+val closeBracket = symbolRule(")")
+val closeCurlyBracket = symbolRule("}")
+val semicolon = symbolRule(";")
 
 private def stringMatchingRule(t: Tokeniser, stringsToMatch: List[String], transformer: String => LexicalElem): MaybeLexicalElements =
   val s = t.currentToken
@@ -34,96 +26,77 @@ private def stringMatchingRule(t: Tokeniser, stringsToMatch: List[String], trans
   else
     Left(s"expected one of $stringsToMatch, got ${t.currentToken}")
 
-case class ObjectRule(rules: List[CompilationRule], enclosingElem: Option[String] = None) extends CompilationRule {
-  override def compile(t: Tokeniser): MaybeLexicalElements =
-    compileWithRules(t, rules, enclosingElem)
+def objectRule(rules: List[RuleTransformer], enclosingElem: Option[String] = None)(t: Tokeniser) =
+  compileWithRules(t, rules, enclosingElem)
+  
+def keywordMatchingOneOfRule(stringsToMatch: List[String])(t: Tokeniser) =
+  stringMatchingRule(t, stringsToMatch, Keyword.apply)
+  
+def symbolMatchingOneOfRule(symbolsToMatch: List[String])(t: Tokeniser) =
+  stringMatchingRule(t, symbolsToMatch, (s: String) => Symbol(s.head))
+
+val varRule = (t: Tokeniser) => {
+  val s = t.currentToken
+  TokenTypes.tokenType(s) match
+    case TokenTypes.Identifier => t.safeAdvance.flatMap(_ => Right(List(Identifier(s))))
+    case otherTokenType => Left(s"expected token type identifier for string $s but got $otherTokenType")
 }
 
-case class KeywordMatchingOneOfRule(stringsToMatch: List[String]) extends CompilationRule {
-  override def compile(t: Tokeniser): MaybeLexicalElements =
-    stringMatchingRule(t, stringsToMatch, Keyword.apply)
+val expressionListRule = (t: Tokeniser) => {
+  compileExpressionList(t).map(_._1) //TODO
 }
 
-case class SymbolMatchingOneOfRule(symbolsToMatch: List[String]) extends CompilationRule {
-  override def compile(t: Tokeniser): MaybeLexicalElements =
-    stringMatchingRule(t, symbolsToMatch, (s: String) => Symbol(s.head))
-}
-
-case object VarRule extends CompilationRule {
-  override def compile(t: Tokeniser): MaybeLexicalElements =
-    val s = t.currentToken
-    TokenTypes.tokenType(s) match
-      case TokenTypes.Identifier => t.safeAdvance.flatMap(_ => Right(List(Identifier(s))))
-      case otherTokenType => Left(s"expected token type identifier for string $s but got $otherTokenType")
-}
-
-case class CustomRule(rule: Tokeniser => MaybeLexicalElements) extends CompilationRule {
-  override def compile(t: Tokeniser): MaybeLexicalElements = rule(t)
-}
-
-case object ExpressionListRule extends CompilationRule {
-  override def compile(t: Tokeniser): MaybeLexicalElements = compileExpressionList(t).map(_._1)
-}
-
-case class TypeRule(includeVoid: Boolean = false) extends CompilationRule {
-  override def compile(t: Tokeniser): MaybeLexicalElements =
-    val currentToken = t.currentToken
-    TokenTypes.tokenType(currentToken) match {
-      case TokenTypes.Keyword =>
-        val variableTypes = List("boolean", "char", "int")
-        val typesToCheck = if (includeVoid) "void" +: variableTypes else variableTypes
-        if (typesToCheck.contains(currentToken))
-          t.safeAdvance.flatMap(_ => Right(List(Keyword(currentToken))))
-        else
-          Left(s"keyword $currentToken cannot be used as a type, valid keyword types are boolean char or int")
-      case TokenTypes.Identifier =>
-        Right(List(Identifier(currentToken))).tap(_ => t.advance())
-      case other =>
-        Left(s"$currentToken cannot be used as a type")
-    }
-}
-
-case class OptionalElemRule(condition: String => Boolean, rule: Tokeniser => MaybeLexicalElements, advanceIfConditionMet: Boolean = true) extends CompilationRule {
-  override def compile(t: Tokeniser) =
-    if (condition(t.currentToken))
-      if (advanceIfConditionMet)
-        t.safeAdvance.flatMap(_ => rule(t))
-      else rule(t)
-    else
-      Right(List())
-}
-
-case class ZeroOrMoreRule(startTokens: List[String], transformer: Tokeniser => MaybeLexicalElements, enclosingTag: Option[String] = None) extends CompilationRule {
-  override def compile(t: Tokeniser) =
-    @tailrec
-    def go(soFar: List[LexicalElem]): MaybeLexicalElements =
-      if (!startTokens.contains(t.currentToken)) {
-        enclosingTag match
-          case Some(tag) => Right(encloseWithTags(tag, soFar))
-          case None => Right(soFar)
-      } else {
-        val result = transformer(t)
-        result match
-          case Left(err) => Left(err)
-          case Right(newElems) => go(soFar ++ newElems)
-      }
-
-    go(List())
-}
-
-case class BranchingRule(ruleMap: Map[String, Tokeniser => MaybeLexicalElements]) extends CompilationRule {
-  override def compile(t: Tokeniser) = {
-    ruleMap.get(t.currentToken) match {
-      case Some(transformer) => transformer(t)
-      case None => Left(s"uh oh tried to perform branching rule for possibilities ${ruleMap.keys} with token ${t.currentToken}")
-    }
+def typeRule(includeVoid: Boolean = false)(t: Tokeniser) =
+  val currentToken = t.currentToken
+  TokenTypes.tokenType(currentToken) match {
+    case TokenTypes.Keyword =>
+      val variableTypes = List("boolean", "char", "int")
+      val typesToCheck = if (includeVoid) "void" +: variableTypes else variableTypes
+      if (typesToCheck.contains(currentToken))
+        t.safeAdvance.flatMap(_ => Right(List(Keyword(currentToken))))
+      else
+        Left(s"keyword $currentToken cannot be used as a type, valid keyword types are boolean char or int")
+    case TokenTypes.Identifier =>
+      Right(List(Identifier(currentToken))).tap(_ => t.advance())
+    case other =>
+      Left(s"$currentToken cannot be used as a type")
   }
-}
 
+def optionalElemRule(condition: String => Boolean, rule: Tokeniser => MaybeLexicalElements, advanceIfConditionMet: Boolean = true)(t: Tokeniser) =
+  if (condition(t.currentToken))
+    if (advanceIfConditionMet)
+      t.safeAdvance.flatMap(_ => rule(t))
+    else rule(t)
+  else
+    Right(List())
 
-def compileWithRules(t: Tokeniser, rules: List[CompilationRule], enclosingElem: Option[String]): MaybeLexicalElements =
+def zeroOrMoreRule(startTokens: List[String], transformer: Tokeniser => MaybeLexicalElements, enclosingTag: Option[String] = None)(t: Tokeniser) =
   @tailrec
-  def go(soFar: List[LexicalElem], remainingRules: List[CompilationRule]): MaybeLexicalElements =
+  def go(soFar: List[LexicalElem]): MaybeLexicalElements =
+    if (!startTokens.contains(t.currentToken)) {
+      enclosingTag match
+        case Some(tag) => Right(encloseWithTags(tag, soFar))
+        case None => Right(soFar)
+    } else {
+      val result = transformer(t)
+      result match
+        case Left(err) => Left(err)
+        case Right(newElems) => go(soFar ++ newElems)
+    }
+
+  go(List())
+  
+def branchingRule(ruleMap: Map[String, RuleTransformer])(t: Tokeniser) =
+  ruleMap.get(t.currentToken) match {
+    case Some(transformer) => transformer(t)
+    case None => Left(s"uh oh tried to perform branching rule for possibilities ${ruleMap.keys} with token ${t.currentToken}")
+  }
+  
+type RuleTransformer = Tokeniser => MaybeLexicalElements 
+
+def compileWithRules(t: Tokeniser, rules: List[RuleTransformer], enclosingElem: Option[String]): MaybeLexicalElements =
+  @tailrec
+  def go(soFar: List[LexicalElem], remainingRules: List[RuleTransformer]): MaybeLexicalElements =
     remainingRules match
       case Nil =>
         enclosingElem match {
@@ -131,7 +104,7 @@ def compileWithRules(t: Tokeniser, rules: List[CompilationRule], enclosingElem: 
           case None => Right(soFar)
         }
       case firstRule :: otherRules =>
-        firstRule.compile(t) match
+        firstRule(t) match
           case Left(err) => Left(err)
           case Right(newElems) => go(soFar ++ newElems, otherRules)
 
